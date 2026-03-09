072b96964649be9a82758db3dc5559ff971c7287
vincent-nicolau <ven00002@mix.wvu.edu>
2026-03-09 10:29:01 -0400

    Add project files.

---
+++ RazorPagesMovie/Models/Movie.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RazorPagesMovie.Models;
+
+public class Movie
+{
+    public int Id { get; set; }
+
+    [StringLength(60, MinimumLength = 3)]
+    [Required]
+    public string Title { get; set; } = string.Empty;
+
+    [DataType(DataType.Date)]
+    [Required]
+    public DateTime ReleaseDate { get; set; }
+
+    [Range(1, 100)]
+    [DataType(DataType.Currency)]
+    [Column(TypeName = "decimal(18, 2)")]
+    public decimal Price { get; set; }
+
+    [RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$")]
+    [Required]
+    [StringLength(30)]
+    public string Genre { get; set; } = string.Empty;
+
+    [Required]
+    [Range(1, 10, ErrorMessage = "Must be between 1 and 10.")]
+    public int Rank { get; set; }
+}
